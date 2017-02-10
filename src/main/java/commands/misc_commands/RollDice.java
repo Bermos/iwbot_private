ef82cd6cc6921b14695c9875febbaa1e82302c3f
Bermos <matthew.machivenyika@hispeed.ch>
2017-02-10 01:32:36 +0100

    Bugfix for /roll

--- src/main/java/commands/misc_commands/RollDice.java
+++ src/main/java/commands/misc_commands/RollDice.java
@@ -20,10 +20,26 @@ public class RollDice implements PMCommand, GuildCommand{
     @Override
     public void runCommand(GuildMessageReceivedEvent event, String[] args) {
         int max = 6;
-        if (args.length == 1)
-            max = Integer.parseInt(args[0]);
+        boolean negative = false;
+        if (args.length == 1) {
+            try {
+                max = Integer.parseInt(args[0]);
 
-        event.getChannel().sendMessage("Your rolled a " + roll(max)).queue();
+                if (max == 0) {
+                    event.getChannel().sendMessage("You rolled a 0, surprise dumbass!");
+                    return;
+                } else if (max < 0) {
+                    negative = true;
+                    max = 0 - max;
+                }
+            } catch (NumberFormatException e) {
+                event.getChannel().sendMessage("[Error] " + args[0] + " is not a valid number.");
+            }
+        }
+
+        int roll = negative ? 0 - roll(max) : roll(max);
+
+        event.getChannel().sendMessage("Your rolled a " + roll).queue();
     }
 
     @Override
