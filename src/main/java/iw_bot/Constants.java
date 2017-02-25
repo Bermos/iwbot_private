1981df268f9b0e938c1dbed5ef3fa1559f5c181a
nobkins <admin@rwdecal.co.uk>
2017-02-25 01:25:35 +0000

    Further fix to help for /bgs goal,notes

--- src/main/java/iw_bot/Constants.java
+++ src/main/java/iw_bot/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
             "To add an activity target to a goal use " + Listener.prefix + "bgs goal, addactivity";
     public final static String BGS_GOAL_EDITACT_HELP = "**Help: Add/Edit BGS Goal Activity Targets**\n" + Listener.prefix + "bgs goal, {addactivity, addact, editactivity, editact}, <goalid> <Activity/CMDRGoal #/GlobalGoal #>\n"+
             "You can append as many <Activity/CMDRGoal #/GlobalGoal #> to the command as you require. Just seperate them by commas.";
-    public final static String BGS_GOAL_NOTE_HELP = "**Help: List BGS Goals**" + Listener.prefix + "bgs goal, note, <goalid>, <text>\n"+
+    public final static String BGS_GOAL_NOTE_HELP = "**Help: List BGS Goals**\n" + Listener.prefix + "bgs goal, note, <goalid>, <text>\n"+
             "Add a note to a goal with specific instructions for CMDRs";
 
 
